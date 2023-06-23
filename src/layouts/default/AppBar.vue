86fee56db38ac040de62a5fb58ea90b087cbe6e1
Lucas Gautério <lucasgauterio@cvccorp.com.br>
2023-06-23 20:17:32 -0300

    project branding

--- src/layouts/default/AppBar.vue
+++ src/layouts/default/AppBar.vue
@@ -1,13 +1,16 @@
 <template>
   <v-app-bar app>
     <v-app-bar-title>
-      <v-icon icon="mdi-circle-slice-4" />
-
-      GPT Developer
+      <v-img :src="logo" title="GPT Developer" height="60px"></v-img>
     </v-app-bar-title>
   </v-app-bar>
 </template>
 
-<script setup>
-  //
+<script>
+import logo from '../../assets/logo.svg'
+export default {
+  data: () => ({
+    logo: logo,
+  }),
+}
 </script>
